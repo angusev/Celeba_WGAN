6a6bf3b2abe7a6a22e386601432fa3332d6f4eb2
andrey gusev <44449418+angusev@users.noreply.github.com>
2021-01-11 20:36:13 +0300

    remove previous model

--- code/model.py
+++ code/model.py
@@ -28,55 +28,6 @@ class PrintLayer(nn.Module):
 #         print(x.shape)
         return x
     
-
-# class Generator(nn.Module):
-#     def __init__(self):
-#         super(Generator, self).__init__()
-        
-#         def block(in_feat, out_feat, normalize=True):
-#             layers = [nn.Linear(in_feat, out_feat)]
-#             if normalize:
-#                 layers.append(nn.BatchNorm1d(out_feat, 0.8))
-#             layers.append(nn.LeakyReLU(0.2, inplace=True))
-#             return layers
-
-#         self.model = nn.Sequential(
-#             *block(80, 128, normalize=False),
-#             *block(128, 256),
-#             *block(256, 512),
-#             *block(512, 1024),
-#             nn.Linear(1024, int(np.prod(img_shape))),
-#             nn.Tanh()
-#         )
-
-#     def forward(self, conditions):
-#         noise = torch.randn(conditions.shape).to('cuda')
-#         to_input = torch.cat((conditions, noise), 1)
-#         img = self.model(to_input)
-#         img = img.view(img.shape[0], *img_shape)
-#         return img
-
-
-# class Discriminator(nn.Module):
-#     def __init__(self):
-#         super(Discriminator, self).__init__()
-        
-#         self.model = nn.Sequential(
-#             nn.Linear(int((img_shape[0] + 1) * np.prod(img_shape[1:])), 512),
-#             nn.LeakyReLU(0.2, inplace=True),
-#             nn.Linear(512, 256),
-#             nn.LeakyReLU(0.2, inplace=True),
-#             nn.Linear(256, 1),
-#         )
-
-#     def forward(self, images, conditions):
-#         cond = F.interpolate(conditions.unsqueeze(1).unsqueeze(dim=3),
-#                              size=img_shape[1:]).squeeze(dim=3)
-#         to_input = torch.cat((cond, images), 1)
-#         img_flat = to_input.view(to_input.shape[0], -1)
-#         validity = self.model(img_flat)
-#         return validity
-    
     
 class Generator(nn.Module):
     def __init__(self):
