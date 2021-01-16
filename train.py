d88c787ec65b8785f0c3f3b73fe69cba0d72ebcd
Andrey Gusev <aagusev_2@edu.hse.ru>
2021-01-16 18:04:04 +0000

    black linter

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import sys
 from PIL import Image
 
 import torch
+
 # torch.multiprocessing.set_sharing_strategy('file_system')
 import torch.nn as nn
 from torch import optim
@@ -13,8 +14,7 @@ from torchvision import models
 
 from code.dataclasses import get_dataloaders
 from code.model import WGAN, weights_init
-from code.utils import (get_image_files,
-                        show_random_examples)
+from code.utils import get_image_files, show_random_examples
 from code.trainer import train, test
 from code.configs import Parser
 
@@ -33,17 +33,19 @@ DEVICE = torch.device("cuda")
 
 
 def main(args):
-#     set_seed(179)
-    
+    #     set_seed(179)
+
     parser = Parser()
     paths, training_configs, launch_configs = parser.parse_args(args)
     training_configs.device = DEVICE
 
     if launch_configs.wandb:
         wandb.init(project="celeba_wgan", name=launch_configs.wandb)
-    
-    train_dataset, valid_dataset, train_loader, valid_loader = get_dataloaders(paths, training_configs, launch_configs)
-    
+
+    train_dataset, valid_dataset, train_loader, valid_loader = get_dataloaders(
+        paths, training_configs, launch_configs
+    )
+
     model = WGAN(training_configs).to(DEVICE)
     weights_init(model)
 
@@ -51,23 +53,31 @@ def main(args):
         for epoch in range(training_configs.epochs):
             train_losses = train(model, DEVICE, train_loader)
             test_losses = test(model, DEVICE, valid_loader)
-            
-            print(f'Epoch {epoch + 1} / {training_configs.epochs} \t  [loss_D, loss_G]')
-            print(f'\tTrain losses: {[np.round(l.item(), 4) for l in train_losses]}')
-            print(f'\tValid loss:   {[np.round(l.item(), 4) for l in test_losses]}')
-            
-            show_random_examples(model.model_G, valid_dataset, DEVICE, paths.examples / f'epoch{epoch + 1}.jpg')
+
+            print(f"Epoch {epoch + 1} / {training_configs.epochs} \t  [loss_D, loss_G]")
+            print(f"\tTrain losses: {[np.round(l.item(), 4) for l in train_losses]}")
+            print(f"\tValid loss:   {[np.round(l.item(), 4) for l in test_losses]}")
+
+            show_random_examples(
+                model.model_G,
+                valid_dataset,
+                DEVICE,
+                paths.examples / f"epoch{epoch + 1}.jpg",
+            )
             if launch_configs.wandb:
                 train_loss_D, train_loss_G = train_losses
                 test_loss_D, test_loss_G = test_losses
-                wandb.log({"train_loss_D": train_loss_D,
-                           "train_loss_G": train_loss_G,
-                           "test_loss_D": test_loss_D,
-                           "test_loss_G": test_loss_G
-                          })
+                wandb.log(
+                    {
+                        "train_loss_D": train_loss_D,
+                        "train_loss_G": train_loss_G,
+                        "test_loss_D": test_loss_D,
+                        "test_loss_G": test_loss_G,
+                    }
+                )
     except KeyboardInterrupt:
-        print('\nBye')
-  
+        print("\nBye")
+
 
 if __name__ == "__main__":
     main(sys.argv)
